5b82f0102bc47c1a677c5395a315beec81c7d541
Vitor Lucas <vitorluc.2013@gmail.com>
2026-03-03 08:52:19 -0300

    Project created

---
+++ src/main/java/vbotelho/dev/algorithm_sm2/AlgorithmSm2Application.java
@@ -0,0 +1,13 @@
+package vbotelho.dev.algorithm_sm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmSm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmSm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vbotelho/dev/algorithm_sm2/AlgorithmSm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vbotelho.dev.algorithm_sm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmSm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
