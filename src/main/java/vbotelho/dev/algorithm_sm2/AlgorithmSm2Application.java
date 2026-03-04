7fb1a5126e81e40cc01adb742c64bcb642630919
Vitor Lucas <vitorluc.2013@gmail.com>
2026-03-04 12:21:29 -0300

    Enable scheduling configuration

--- src/main/java/vbotelho/dev/algorithm_sm2/AlgorithmSm2Application.java
+++ src/main/java/vbotelho/dev/algorithm_sm2/AlgorithmSm2Application.java
@@ -2,8 +2,10 @@ package vbotelho.dev.algorithm_sm2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlgorithmSm2Application {
 
 	public static void main(String[] args) {
